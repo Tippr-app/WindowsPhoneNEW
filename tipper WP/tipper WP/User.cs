20672a728d70fe25edd1078cc77a2a3739122189
Morris Lowitz <morris.lowitz@gmail.com>
2015-05-22 15:31:03 -0400

    fgf

---
+++ tipper WP/tipper WP/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace tipper_WP.Data
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Complete { get; set; }
+    }
+}
